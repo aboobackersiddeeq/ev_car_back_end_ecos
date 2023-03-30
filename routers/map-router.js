c8ff0fde79c96673d0ff24b8f041be480722ee82
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-03-30 16:17:40 +0530

    map router added

---
+++ routers/map-router.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mapController = require("../controllers/map-controllers");
+const router = express.Router();
+
+router.post("/add-map", mapController.addMap);
+ 
+router.get("/get-map",  mapController.getMap);
+router.post("/get-map-near",  mapController.getPlaceNear);
+
+
+
+module.exports = router;
