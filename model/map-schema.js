0ada8b6ad28578f16358801bde4bb43b3e271239
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-03-30 16:17:03 +0530

    map schema added

---
+++ model/map-schema.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const mapSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+      },
+    title: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 60,
+    },
+    desc: {
+      type: String,
+      required: true,
+      min: 3,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    location: {
+        type: { type: String },
+        coordinates: []
+       },
+    long: {
+      type: Number,
+      required: true,
+    },
+    lat: {
+      type: Number,
+      required: true,
+    },
+
+    isBanned: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+mapSchema.index({ location: "2dsphere" });
+const Pin = mongoose.model("map", mapSchema);
+module.exports = Pin;
