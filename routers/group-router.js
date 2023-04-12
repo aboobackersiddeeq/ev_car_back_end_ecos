6b8f3106153a69dd1e8b00a6488e5cb4dad73c95
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-04-12 20:48:36 +0530

    updated

--- routers/group-router.js
+++ routers/group-router.js
@@ -3,11 +3,11 @@ const Controller = require("../controllers/group-controllers");
 const Auth = require("../middlewares/authentication");
 const router = express.Router();
 
-router.post("/new-group",Auth.UserJwt, Controller.newGroup);
-router.post("/get-group",Auth.UserJwt, Controller.getGroups);
-router.post("/get-user-group" ,Auth.UserJwt, Controller.getGroupsOfUser);
-router.post("/join-group" ,Auth.UserJwt, Controller.joinGroup);
-router.post("/get-messages" ,Auth.UserJwt, Controller.messages);
- 
+router.post("/new-group", Auth.UserJwt, Controller.newGroup);
+router.post("/get-group", Auth.UserJwt, Controller.getGroups);
+router.post("/get-user-group", Auth.UserJwt, Controller.getGroupsOfUser);
+router.post("/join-group", Auth.UserJwt, Controller.joinGroup);
+router.post("/get-messages", Auth.UserJwt, Controller.messages);
+router.post("/update-group", Auth.UserJwt, Controller.addPostImage);
 
 module.exports = router;
