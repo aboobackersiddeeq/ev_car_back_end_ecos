73edb0f8cfe8e32269fb2ce64b4caf5c4ae429f8
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-04-12 20:49:44 +0530

    dealer router modified

--- routers/dealer-router.js
+++ routers/dealer-router.js
@@ -6,6 +6,12 @@ const router = express.Router();
 router.post("/", controller.dealerLogin);
 router.get("/isDealerAuth", authentication.dealerJwt, controller.dealerAuth);
 router.get("/get-bookings", authentication.dealerJwt, controller.getBooking);
+router.post("/update-dealer", authentication.dealerJwt, controller.addPostImage);
+router.post("/otp",authentication.dealerJwt, controller.sendOtp);
+router.post("/verify-otp",authentication.dealerJwt,controller.verifyOtp);
+router.post("/old-password",authentication.dealerJwt,controller.verifyOldPassword);
+router.post("/password-update",authentication.dealerJwt,controller.delearPasswordUpdate);
 // dashboard deatils
 router.get("/get-dashboard", authentication.dealerJwt, controller.dashboard);
 module.exports = router;
+ 
