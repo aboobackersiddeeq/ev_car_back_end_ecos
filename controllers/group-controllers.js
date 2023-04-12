6c41050060870e85cdc98bc20a44d93cdccd68a3
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-04-12 20:52:22 +0530

    modified

--- controllers/group-controllers.js
+++ controllers/group-controllers.js
@@ -28,7 +28,7 @@ module.exports = {
   },
   getGroups: (req, res) => {
     Group.find({ "members.id": { $ne: req.body.id } })
-      .sort({ messageUpdate: -1 })
+      .sort({ updatedAt: -1, messageUpdate: -1, createdAt: -1 })
       .then((groups) => res.json({ result: groups, status: "success" }))
       .catch((err) =>
         res.json({ status: "failed", message: "could not get groups", err })
@@ -36,7 +36,7 @@ module.exports = {
   },
   getGroupsOfUser: (req, res) => {
     Group.find({ "members.id": req.body.id })
-      .sort({ messageUpdate: -1 })
+      .sort({ updatedAt: -1, messageUpdate: -1, createdAt: -1 })
       .then((groups) => res.json({ result: groups, status: "success" }))
       .catch((err) =>
         res.json({ status: "failed", message: "could not get groups", err })
@@ -58,10 +58,11 @@ module.exports = {
       );
       await groups
         .save()
-        .then(() =>{
-            Group.find({ "members.id":  req.body.userId })
+        .then(() => {
+          Group.find({ "members.id": req.body.userId })
             .sort({ messageUpdate: -1 })
-            .then((groups) => res.json({ result: groups, status: "success" }))})
+            .then((groups) => res.json({ result: groups, status: "success" }));
+        })
         .catch((err) =>
           res.json({ status: "failed", message: "could not get groups", err })
         );
@@ -69,13 +70,84 @@ module.exports = {
       res.status(400).json({ error: "could not join group", err });
     }
   },
-   messages : async (req, res) => {
+  messages: async (req, res) => {
     try {
-        await Messages.find({ group: req.body.groupId })
-            .then((groupMsg) => res.json({ result: groupMsg, status: "success" }))
-            .catch((err) => res.json({ status: "failed", message: "could not get group messages", err }));
+      await Messages.find({ group: req.body.groupId })
+        .then((groupMsg) => res.json({ result: groupMsg, status: "success" }))
+        .catch((err) =>
+          res.json({
+            status: "failed",
+            message: "could not get group messages",
+            err,
+          })
+        );
     } catch (err) {
-        res.status(400).json({ status: "failed", message: "could not get group messages", err });
+      res.status(400).json({
+        status: "failed",
+        message: "could not get group messages",
+        err,
+      });
     }
-}
+  },
+  editGrpDp: async (req, res) => {
+    try {
+      Group.findOneAndUpdate(
+        {
+          _id: req.body.id,
+        },
+        {
+          $set: {
+            image: req.file.path,
+          },
+        }
+      )
+        .then((data) => res.json(data))
+        .catch((err) => res.json({ error: "could not edit dp", err }));
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  editGrpName: async (req, res) => {
+    try {
+      Group.findOneAndUpdate(
+        {
+          _id: req.body.id,
+        },
+        {
+          $set: {
+            groupName: req.body.grpName,
+          },
+        }
+      )
+        .then((data) => res.json(data))
+        .catch((err) => res.json({ error: "could not edit grp name", err }));
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  addPostImage: async (req, res) => {
+    try {
+      const id = req.body.id;
+      const image = req.files.img;
+      let imageUrl;
+      if (image) {
+        imageUrl = image[0].path;
+        imageUrl = imageUrl.substring(6);
+      }
+      await Group.findByIdAndUpdate(id, {
+        image: imageUrl,
+        groupName: req.body.groupName,
+      })
+        .then(async () => {
+          const Details = await Group.findOne({ _id: id });
+          res.json({ status: "success", result: Details });
+        })
+        .catch((error) => {
+          res.json({ status: "failed", message: error.message });
+        });
+    } catch (error) {
+      res.json({ status: "failed", message: error.message });
+    }
+  },
 };
