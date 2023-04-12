a12a846c3bd49d923dcfae7658c6078d65d3dd6d
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-04-12 20:47:08 +0530

    update and new router added

--- routers/map-router.js
+++ routers/map-router.js
@@ -3,7 +3,6 @@ const mapController = require("../controllers/map-controllers");
 const router = express.Router();
 
 router.post("/add-map", mapController.addMap);
-
 router.get("/get-map", mapController.getMap);
 router.post("/get-map-near", mapController.getPlaceNear);
 
