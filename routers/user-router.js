df883e40e5e405463dc4d9eccffad174eb92ee86
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-06-06 11:59:56 +0530

    OTP routers added

--- routers/user-router.js
+++ routers/user-router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userController = require("../controllers/user-controllers");
 const bookingController = require("../controllers/booking-controllers");
 const testDriveController = require("../controllers/test-drive-controllers");
+const otpController = require("../controllers/otp-controllers");
 const userAuth = require("../middlewares/authentication");
 const router = express.Router();
 //user Details
@@ -9,6 +10,9 @@ router.post("/login", userController.userlogin);
 router.post("/login-with-google", userController.userloginWithGoogle);
 router.post("/signup", userController.usersignup);
 router.get("/is-user-auth", userAuth.UserJwt, userController.isUserAuth);
+// OTP 
+router.post("/otp", otpController.sendOtp);
+router.post("/verify",otpController.verifyOtp);
 //test Drive
 router.post("/test-drive", testDriveController.testDriveBooking);
 //booking
